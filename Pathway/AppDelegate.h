832fe0ed823e856166c1df5633c0d4e89949a455
Ajay Kirtikar <AjayKirtikar@Sanjay-Kirtikars-iMac.local>
2014-03-21 20:05:27 -0500

    Initial Commit

---
+++ Pathway/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Pathway
+//
+//  Created by Ajay Kirtikar on 3/21/14.
+//  Copyright (c) 2014 K Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pathway/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pathway
+//
+//  Created by Ajay Kirtikar on 3/21/14.
+//  Copyright (c) 2014 K Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
